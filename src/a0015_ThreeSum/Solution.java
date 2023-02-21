ac91f711b7784d364bf1615df6b7aa8cc03fc3d6
liwanjun <liwanjun@inspur.com>
2023-02-21 17:15:18 +0800

    update 2023/02/21

--- src/a0015_ThreeSum/Solution.java
+++ src/a0015_ThreeSum/Solution.java
@@ -22,15 +22,13 @@ import java.util.List;
 public class Solution {
 
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> results = new ArrayList<List<Integer>>();
+        List<List<Integer>> results = new ArrayList<>();
         Arrays.sort(nums);
-        int len = nums.length;
-        for (int i = 0; i < len - 2; i++) {
+        for (int i = 0; i < nums.length - 2; i++) {
             if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
-            int j = i + 1;
-            int k = len - 1;
+            int j = i + 1, k = nums.length - 1;
             while (j < k) {
                 int sum = nums[i] + nums[j] + nums[k];
                 if (sum == 0) {
