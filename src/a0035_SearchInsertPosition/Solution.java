f2cfe80885d8f8c676799ade840fd581ad5f5087
liwanjun <liwanjun@inspur.com>
2023-02-22 16:26:42 +0800

    update 2023/02/22

--- src/a0035_SearchInsertPosition/Solution.java
+++ src/a0035_SearchInsertPosition/Solution.java
@@ -22,13 +22,16 @@ package a0035_SearchInsertPosition;
 public class Solution {
 
     public int searchInsert(int[] nums, int target) {
-        int lo = 0, hi = nums.length;
-        while (lo < hi) {
+        int lo = 0, hi = nums.length - 1;
+        while (lo <= hi) {
             int mid = lo + (hi - lo) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
             if (nums[mid] < target) {
                 lo = mid + 1;
             } else {
-                hi = mid;
+                hi = mid - 1;
             }
         }
         return lo;
