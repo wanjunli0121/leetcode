6366d35d7f34b01f14a7e967056423f8428e0dc1
liwanjun <liwanjun@didiglobal.com>
2024-05-14 19:54:02 +0800

    update 2024/05/14

--- src/a0091_DecodeWays/Solution.java
+++ src/a0091_DecodeWays/Solution.java
@@ -18,23 +18,23 @@ package a0091_DecodeWays;
 public class Solution {
 
     public int numDecodings(String s) {
-        if (s == null || s.length() == 0) {
-            return 0;
-        }
-        int[] dp = new int[s.length() + 1];
-        dp[0] = 1;
-        dp[1] = s.charAt(0) == '0' ? 0 : 1;
-        for (int i = 2; i <= s.length(); i++) {
-            int oneDigit = Integer.parseInt(s.substring(i - 1, i));
-            int twoDigits = Integer.parseInt(s.substring(i - 2, i));
-            if (oneDigit >= 1 && oneDigit <= 9) {
-                dp[i] += dp[i - 1];
+        int pre = 1, cur = 1;
+        for (int i = 0; i < s.length(); i++) {
+            int t = 0;
+            char c1 = s.charAt(i);
+            if (c1 != '0') {
+                t += cur;
             }
-            if (twoDigits >= 10 && twoDigits <= 26) {
-                dp[i] += dp[i - 2];
+            if (i > 0) {
+                char c2 = s.charAt(i - 1);
+                if (c2 != '0' && (c2 - '0') * 10 + (c1 - '0') <= 26) {
+                    t += pre;
+                }
             }
+            pre = cur;
+            cur = t;
         }
-        return dp[s.length()];
+        return cur;
     }
 
 }
